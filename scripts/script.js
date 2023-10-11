9d1137dd82f5cbe4d5f29ef90177eb1966dbb1c5
Treppord <droppertjasper@gmail.com>
2023-10-11 10:21:00 +0200

    Intergrated nav

--- scripts/script.js
+++ scripts/script.js
@@ -34,3 +34,26 @@ document.querySelectorAll(".like").forEach((button, index) => {
     closePopup();
   });
 });
+
+// Event listener for all frame-view-buttons
+document.querySelectorAll(".sidebarmenu").forEach((button, index) => {
+  button.addEventListener("click", () => {
+    showSidebar();
+  });
+});
+
+// Event listener for all frame-view-buttons
+document.querySelectorAll(".sidebarexit").forEach((button, index) => {
+  button.addEventListener("click", () => {
+    hideSidebar();
+  });
+});
+
+function showSidebar() {
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.style.display = "flex";
+}
+function hideSidebar() {
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.style.display = "none";
+}
